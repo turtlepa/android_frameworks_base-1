d6a71c6c491e0cc535bf9c2b79034b286c6a339d
Janson Kang <temasek71@gmail.com>
2013-01-03 11:45:05 +0800

    Revert "Fix compile error - Center Clock"
    This reverts commit 3153d39e1cc5bd2200f1f20db369e4f6db127eb2.

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -1293,16 +1293,21 @@ public class PhoneStatusBar extends BaseStatusBar {
     public void showClock(boolean show) {
         if (mStatusBarView == null) return;
         ContentResolver resolver = mContext.getContentResolver();
+        View clock = mStatusBarView.findViewById(R.id.clock);
         mClockStyle = (Settings.System.getInt(resolver,Settings.System.STATUS_BAR_CLOCK_STYLE, 1));
         Clock clock = (Clock) mStatusBarView.findViewById(R.id.clock);
         CenterClock cclock = (CenterClock) mStatusBarView.findViewById(R.id.center_clock);
         if(mClockStyle != 0 && clock !=null && cclock != null){
-            clock.updateClockVisibility(show);     
+            clock.updateClockVisibility(show);
             cclock.updateClockVisibility(show);
         }
         else{
-            clock.updateClockVisibility(false);
-            cclock.updateClockVisibility(false);
+            if (clock != null) {
+                clock.updateClockVisibility(false);
+            }
+            if (cclock != null) {
+                cclock.updateClockVisibility(false);
+            }
         }
     }
 
