f50a15856103ec30ace39d2085d457e697a938bf
Jon Anders Skorpen <jaskorpe@mindmutation.net>
2013-01-27 11:28:45 +0100

    SystemUI: Option to display Quick Settings when there are no ongoing notificaions
    Added member function hasClearableNotifications to class
    PhoneStatusBar. This function returns true when clearable
    notifications are present on the status bar. Also, onTouchEvent in
    NotificaionPanelView now flips to Quick Settings if there are no
    ongoing notifications, and the QS_QUICK_PULLDOWN is set accordingly.
    
    Change-Id: Ia77f90b19c7b408b7fc9376b6914e3173b734655

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -1241,6 +1241,10 @@ public class PhoneStatusBar extends BaseStatusBar {
         }
     }
 
+    boolean hasClearableNotifications() {
+        return mNotificationData.hasClearableItems();
+    }
+
     @Override
     protected void setAreThereNotifications() {
         final boolean any = mNotificationData.size() > 0;
