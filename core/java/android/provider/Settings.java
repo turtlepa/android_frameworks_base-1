96c9c525e85531aaafa661da2ca34feed0227c9d
Adam Fisch <adammfisch@gmail.com>
2013-02-14 22:44:30 -0600

    Framework: add battery bar (2/2)
    Change-Id: I4f0d65a4b9673a63ed44a32619c1dba882f5e9ea
    Screenshot: http://i.imgur.com/JSnr5tn.png
    
    Conflicts:
    
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2914,6 +2914,32 @@ public final class Settings {
         public static final String NOTIFICATION_SHADE_DIM = "notification_shade_dim";
 
         /**
+         * Whether to show the battery bar
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR = "statusbar_battery_bar";
+
+        /**
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR_COLOR = "statusbar_battery_bar_color";
+
+        /**
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR_THICKNESS = "statusbar_battery_bar_thickness";
+
+        /**
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR_STYLE = "statusbar_battery_bar_style";
+
+        /**
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR_ANIMATE = "statusbar_battery_bar_animate";
+
+        /**
          * Show the pending notification counts as overlays on the status bar
          * @hide
          */
