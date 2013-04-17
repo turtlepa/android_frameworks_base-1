02dc243f42908b68b47c35ee1bbe47632f1b5685
Janson Kang <temasek71@gmail.com>
2013-04-17 22:19:55 +0800

    Fix derp

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkController.java
@@ -1169,10 +1169,6 @@ public class NetworkController extends BroadcastReceiver {
         if (customLabel != null && customLabel.trim().length() > 0) {
             combinedLabel = customLabel;
             mobileLabel = customLabel;
-            if (Settings.System.getInt(mContext.getContentResolver(),
-                    Settings.System.NOTIFICATION_SHOW_WIFI_SSID, 0) == 0) {
-                wifiLabel = customLabel;
-            }
         }
 
         if (DEBUG) {
