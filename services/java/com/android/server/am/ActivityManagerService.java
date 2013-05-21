4a2c5883d5fe7761d812205fc0119a5b8167df63
Jun Su <juns@nvidia.com>
2013-05-21 17:24:32 +0800

    framework:base: iterator error in singletone provider
    In case of singletone provider, we don't count it in the guset user.
    After we remove it from the array, and resize the array, need to move
    iterator back.
    
    Change-Id: I99920a6beccc9de0e67ffeb632b1940efaee10b9

--- services/java/com/android/server/am/ActivityManagerService.java
+++ services/java/com/android/server/am/ActivityManagerService.java
@@ -6277,6 +6277,7 @@ public final class ActivityManagerService extends ActivityManagerNative
                     // it runs in the process of the default user.  Get rid of it.
                     providers.remove(i);
                     N--;
+                    i--;
                     continue;
                 }
 
