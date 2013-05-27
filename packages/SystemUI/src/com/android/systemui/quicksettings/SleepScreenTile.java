a55365bcbc69915776b70033340f48e2cc77585e
Janson Kang <temasek71@gmail.com>
2013-05-27 14:09:53 +0800

    Revert "Power'full' Sleep [1/2]"
    This reverts commit d9d54f7aa6e810994d9fd1f8bd0ab31e226ac941.

--- packages/SystemUI/src/com/android/systemui/quicksettings/SleepScreenTile.java
+++ packages/SystemUI/src/com/android/systemui/quicksettings/SleepScreenTile.java
@@ -1,10 +1,8 @@
 package com.android.systemui.quicksettings;
 
 import android.content.Context;
-import android.hardware.input.InputManager;
 import android.os.PowerManager;
 import android.os.SystemClock;
-import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -31,27 +29,10 @@ public class SleepScreenTile extends QuickSettingsTile {
 
             @Override
             public boolean onLongClick(View v) {
-                triggerVirtualKeypress(KeyEvent.KEYCODE_POWER, true);
+                startSettingsActivity("android.settings.DISPLAY_SETTINGS");
                 return true;
             }
         };
-    }
-
-    private void triggerVirtualKeypress(final int keyCode, final boolean longPress) {
-        new Thread(new Runnable() {
-            public void run() {
-                InputManager im = InputManager.getInstance();
-                KeyEvent keyEvent;
-                if (longPress) {
-                    keyEvent = new KeyEvent(KeyEvent.ACTION_DOWN, keyCode);
-                    keyEvent.changeFlags(keyEvent, KeyEvent.FLAG_FROM_SYSTEM | KeyEvent.FLAG_LONG_PRESS);
-                } else {
-                    keyEvent = new KeyEvent(KeyEvent.ACTION_UP, keyCode);
-                    keyEvent.changeFlags(keyEvent, KeyEvent.FLAG_FROM_SYSTEM);
-                }
-                im.injectInputEvent(keyEvent, InputManager.INJECT_INPUT_EVENT_MODE_WAIT_FOR_RESULT);
-            }
-        }).start();
     }
 
     @Override
@@ -67,8 +48,8 @@ public class SleepScreenTile extends QuickSettingsTile {
     }
 
     private synchronized void updateTile() {
-        mDrawable = R.drawable.ic_qs_power;
-        mLabel = mContext.getString(R.string.quick_settings_power);
+        mDrawable = R.drawable.ic_qs_sleep;
+        mLabel = mContext.getString(R.string.quick_settings_screen_sleep);
     }
 
 }
