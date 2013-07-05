d9709abf791079bbd6732f6b582e2839efefe90b
Janson Kang <temasek71@gmail.com>
2013-07-05 17:32:02 +0800

    Revert "framework:base: iterator error in singletone provider"
    This reverts commit 4a2c5883d5fe7761d812205fc0119a5b8167df63.

--- services/java/com/android/server/am/ActivityManagerService.java
+++ services/java/com/android/server/am/ActivityManagerService.java
@@ -6352,7 +6352,6 @@ public final class ActivityManagerService extends ActivityManagerNative
                     // it runs in the process of the default user.  Get rid of it.
                     providers.remove(i);
                     N--;
-                    i--;
                     continue;
                 }
 
