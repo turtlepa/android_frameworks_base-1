afde574cdddb55718c0ee01b9d221cdd228fa465
Janson Kang <temasek71@gmail.com>
2013-07-09 09:58:34 +0800

    Revert "Clear loaders array after they are destroyed."
    This reverts commit 96bdd6e00197fe36018f41707e1eaab0b0d3135e.

--- core/java/android/app/LoaderManager.java
+++ core/java/android/app/LoaderManager.java
@@ -833,7 +833,6 @@ class LoaderManagerImpl extends LoaderManager {
             for (int i = mLoaders.size()-1; i >= 0; i--) {
                 mLoaders.valueAt(i).destroy();
             }
-            mLoaders.clear();
         }
         
         if (DEBUG) Log.v(TAG, "Destroying Inactive in " + this);
