be707563cf36eda5f29e363b1e1c4085187a8cb1
Janson Kang <temasek71@gmail.com>
2013-07-09 09:46:33 +0800

    Revert "Avoid possible memory leaks by ensuring stream closures"
    This reverts commit 08e4c129b8cdd2b02f11daf4ce839052cb85f0c5.

--- services/java/com/android/server/DevicePolicyManagerService.java
+++ services/java/com/android/server/DevicePolicyManagerService.java
@@ -800,14 +800,6 @@ public class DevicePolicyManagerService extends IDevicePolicyManager.Stub {
                 // Ignore
             }
             journal.rollback();
-        } finally {
-            try {
-                if (stream != null) {
-                    stream.close();
-                }
-            } catch (IOException ex) {
-                // Ignore
-            }
         }
     }
 
