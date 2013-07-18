20528bb9d694e0860c63be387469279f785f60e0
Scott Brady <sbradymobile@gmail.com>
2013-07-18 11:26:55 -0400

    Frameworks: Option to disable wallpaperservice (1/2)
    Change-Id: Id96ac8fcdf57649214d36c94a4e61835dd021b99

--- services/java/com/android/server/wm/WindowManagerService.java
+++ services/java/com/android/server/wm/WindowManagerService.java
@@ -5120,7 +5120,8 @@ public class WindowManagerService extends IWindowManager.Stub
                 boolean haveWallpaper = false;
                 boolean wallpaperEnabled = mContext.getResources().getBoolean(
                         com.android.internal.R.bool.config_enableWallpaperService)
-                        && !mOnlyCore;
+                        && !mOnlyCore &&
+                        !"0".equals(SystemProperties.get("persist.sys.wallpaperservice", "1"));
                 boolean haveKeyguard = true;
                 // TODO(multidisplay): Expand to all displays?
                 final WindowList windows = getDefaultWindowListLocked();
