0d01af4a101c55b9ed9a9097ecd5463b1994603f
david lamontagne <djlamontagneiii@gmail.com>
2013-08-10 21:42:25 -0400

    FIX
    Change-Id: I2fbd03e054caa03ddf8930a6435b1505d1546fcc

--- packages/SystemUI/src/com/android/systemui/recent/RecentsPanelView.java
+++ packages/SystemUI/src/com/android/systemui/recent/RecentsPanelView.java
@@ -517,12 +517,8 @@ public class RecentsPanelView extends FrameLayout implements OnItemClickListener
                 public boolean onLongClick(View v) {
                     mRecentsContainer.removeAllViewsInLayout();
                     try {
-                        ProcessBuilder pb = new ProcessBuilder("su", "-c", "/system/bin/sh");
-                        OutputStreamWriter osw = new OutputStreamWriter(pb.start().getOutputStream());
-                        osw.write("sync" + "\n" + "echo 3 > /proc/sys/vm/drop_caches" + "\n");
-                        osw.write("\nexit\n");
-                        osw.flush();
-                        osw.close();
+                        Runtime.getRuntime().exec("su -c sync");
+                        Runtime.getRuntime().exec("su -c echo 3 > /proc/sys/vm/drop_caches");
                     } catch (Exception e) {
                         Log.d(TAG, "Flush caches failed!");
                     }
