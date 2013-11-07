420d6d89042515fd9a0e764a7365dbbc67f7df3d
Lars Greiss <kufikugel@googlemail.com>
2013-11-07 20:57:56 -0200

    Customizable Low battery pop-up warning (1/2)
    Change-Id: Iacd26b4b111f853853adeaf900c762313aa3e056

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2280,6 +2280,19 @@ public final class Settings {
         public static final String NOTIFICATION_LIGHT_PULSE = "notification_light_pulse";
 
         /**
+         * Battery warning preferences
+         *
+         * 0 = show dialog + play sound (default)
+         * 1 = fire notification + play sound
+         * 2 = show dialog only
+         * 3 = fire notification only
+         * 4 = play sound only
+         * 5 = none
+         * @hide
+         */
+        public static final String POWER_UI_LOW_BATTERY_WARNING_POLICY = "power_ui_low_battery_warning_policy";
+
+        /**
          * Show pointer location on screen?
          * 0 = no
          * 1 = yes
