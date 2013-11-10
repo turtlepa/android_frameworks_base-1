74269cb22b1b15440a21d90b08faebc38cb508c4
maxwen <max.weninger@gmail.com>
2013-11-10 13:17:37 +0100

    base: show notification count overlay
    Change-Id: Ie4df927531bd8d2869780336b5657f8194141b42

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2834,6 +2834,12 @@ public final class Settings {
         public static final String CLEAR_RECENTS_POSITION = "clear_recents_position"; 
 
         /**
+         * Show the pending notification counts as overlays on the status bar
+         * @hide
+         */
+        public static final String STATUS_BAR_NOTIF_COUNT = "status_bar_notif_count";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
