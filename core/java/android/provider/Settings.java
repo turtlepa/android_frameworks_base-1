bcd9500abc45392a17e35df62e8a247c55f1c595
David LaMontagne <DJLaMontagneIII@gmail.com>
2013-11-14 10:43:43 -0500

    Missing symbols

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -1148,6 +1148,7 @@ public final class Settings {
                 return def;
             }
         }
+
 	/**
 	 * Need to AOKP Custom Systems Animations
 	 *
@@ -2698,6 +2699,26 @@ public final class Settings {
          */
         public static final String STATUS_BAR_CIRCLE_BATTERY_ANIMATIONSPEED =
                 "status_bar_circle_battery_animationspeed";
+
+        /**  
+         * Custom System Animations
+         *  
+         * @hide
+         */  
+        public static final String[] ACTIVITY_ANIMATION_CONTROLS = new String[] {  
+                "activity_open",
+                "activity_close",
+                "task_open",  
+                "task_close",  
+                "task_to_front",
+                "task_to_back",  
+                "wallpaper_open",  
+                "wallpaper_close",  
+                "wallpaper_intra_open",  
+                "wallpaper_intra_close",  
+        };  
+        public static final String ANIMATION_CONTROLS_DURATION = "animation_controls_duration";
+        public static final String ANIMATION_CONTROLS_NO_OVERRIDE = "animation_controls_no_override";
 
         /**
         * Whether to control brightness from status bar
