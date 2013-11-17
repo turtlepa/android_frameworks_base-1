SlimRoms/frameworks_base

fb: Slims navbar and navring customizations
add back our navbar and navring tweeks. This is a completly overworked
version compared to the one we carry about a year now. Small glitches
we had are now all fixed. As well it does not need to register extra observers
code was much slimmed down and improved.

Beside that we adapt the 4.4 navbar changes to our system so that it it
consistent with the new 4.4 UI

This patch adds:
- Navbar up to 5 buttons
- navring up to 5 buttons
- dynamic menu behaviour
- landscape navbar options as well now on tablets
- button glow color
- button icon color
- navrign and navbar user can set system or custom icons
- navbar height and width
- enable/disable navbar and navring
- actions are vor navbar and available for the whole system over SlimActions.java
	back
	custom app or activtiy
	home
	kill app
	last app
	notification drawer
	ime switcher
	power menu
	qs pannel
	recents screen
	screen off
	screenshot
	search
	torch if it is available on the device
- navring has targets for handling vib/mute/sound
- added Color class based on blk_jacks idea which first grey an icon out
  and then color it. This gives way better results. As well it fits now
  perfectly with the new 4.4 UI. Put it on a central place so that it is
  from everywhere available. notification shortcuts adapted to the new class
  place
- complete new icons thanks to kroz who has redone all for xxhdpi
- our slim special swipe drag drop press longpress options support
  like on 4.3 this two features
- a lot lot fixes as well on current google code

Change-Id: I8899930b0c84387145e6749271c676bb37efeab9

core/java/com/android/internal/util/slim/ColorHelper.java
@@ -1,0 +1,60 @@
+/*
+* Copyright (C) 2013 SlimRoms Project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+package com.android.internal.util.slim;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffColorFilter;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+
+public class ColorHelper {
+
+    public static Bitmap getColoredBitmap(Drawable d, int color) {
+        Bitmap colorBitmap = ((BitmapDrawable) d).getBitmap();
+        Bitmap grayscaleBitmap = toGrayscale(colorBitmap);
+        Paint pp = new Paint();
+        PorterDuffColorFilter frontFilter =
+            new PorterDuffColorFilter(color, PorterDuff.Mode.MULTIPLY);
+        pp.setColorFilter(frontFilter);
+        Canvas cc = new Canvas(grayscaleBitmap);
+        cc.drawBitmap(grayscaleBitmap, 0, 0, pp);
+        return grayscaleBitmap;
+    }
+
+    private static Bitmap toGrayscale(Bitmap bmpOriginal) {
+        int width, height;
+        height = bmpOriginal.getHeight();
+        width = bmpOriginal.getWidth();
+
+        Bitmap bmpGrayscale = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        Canvas c = new Canvas(bmpGrayscale);
+        Paint paint = new Paint();
+        ColorMatrix cm = new ColorMatrix();
+        cm.setSaturation(0);
+
+        ColorMatrixColorFilter f = new ColorMatrixColorFilter(cm);
+        paint.setColorFilter(f);
+        c.drawBitmap(bmpOriginal, 0, 0, paint);
+        return bmpGrayscale;
+    }
+
+}

core/java/com/android/internal/util/slim/TorchConstants.java
@@ -1,0 +1,56 @@
+/*
+ * Copyright (C) 2013 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.internal.util.slim;
+
+import android.content.Intent;
+
+public class TorchConstants {
+    /**
+     * Package name of the torch app
+     */
+    public static final String APP_PACKAGE_NAME = "net.cactii.flash2";
+
+    /**
+     * Intent broadcast action for toggling the torch state
+     */
+    public static final String ACTION_TOGGLE_STATE = APP_PACKAGE_NAME + ".TOGGLE_FLASHLIGHT";
+
+    /**
+     * Extra for {@link ACTION_TOGGLE_STATE}:
+     * When toggling to on, use the bright brightness setting
+     * Type: boolean
+     */
+    public static final String EXTRA_BRIGHT_MODE = "bright";
+
+    /**
+     * Intent action for 'torch state changed' broadcast
+     */
+    public static final String ACTION_STATE_CHANGED = APP_PACKAGE_NAME + ".TORCH_STATE_CHANGED";
+
+    /**
+     * Extra for {@link ACTION_STATE_CHANGED}:
+     * Current torch state
+     * Type: integer (0/1)
+     */
+    public static final String EXTRA_CURRENT_STATE = "state";
+
+    /**
+     * Intent for launching the torch application
+     */
+    public static Intent INTENT_LAUNCH_APP = new Intent(Intent.ACTION_MAIN)
+            .setClassName(APP_PACKAGE_NAME, APP_PACKAGE_NAME + ".MainActivity");
+}

packages/SystemUI/src/com/android/systemui/statusbar/phone/ShortcutsWidget.java
@@ -20,23 +20,9 @@ import android.app.ActivityManagerNative;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.ActivityInfo;
-import android.content.pm.PackageManager;
-import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.res.Configuration;
-import android.content.res.Resources;
-import android.content.res.Resources.NotFoundException;
 import android.database.ContentObserver;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
-import android.graphics.ColorMatrix;
-import android.graphics.ColorMatrixColorFilter;
-import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
-import android.graphics.Paint;
-import android.graphics.PorterDuff;
-import android.graphics.PorterDuffColorFilter;
 import android.provider.Settings;
 import android.net.Uri;
 import android.os.Bundle;
@@ -60,6 +46,8 @@ import android.widget.LinearLayout;
 import com.android.internal.util.slim.ButtonsHelper;
 import com.android.internal.util.slim.ButtonConfig;
 import com.android.internal.util.slim.ButtonsConstants;
+import com.android.internal.util.slim.ColorHelper;
+
 import com.android.systemui.R;
 
 import java.io.File;
@@ -275,23 +263,6 @@ public class ShortcutsWidget extends LinearLayout {
         requestLayout();
     }
 
-    private Bitmap toGrayscale(Bitmap bmpOriginal) {
-        int width, height;
-        height = bmpOriginal.getHeight();
-        width = bmpOriginal.getWidth();
-
-        Bitmap bmpGrayscale = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
-        Canvas c = new Canvas(bmpGrayscale);
-        Paint paint = new Paint();
-        ColorMatrix cm = new ColorMatrix();
-        cm.setSaturation(0);
-
-        ColorMatrixColorFilter f = new ColorMatrixColorFilter(cm);
-        paint.setColorFilter(f);
-        c.drawBitmap(bmpOriginal, 0, 0, paint);
-        return bmpGrayscale;
-    }
-
     public void recreateShortcutLayout() {
         removeAllViews();
         determineMargins();
@@ -310,33 +281,20 @@ public class ShortcutsWidget extends LinearLayout {
 
                 // Draw ImageView
                 ImageView iv = new ImageView(mContext);
-                try {
-                    // colorize system and gallery icons always and app icons only if toggled
-                    if ((buttonConfig.getIcon() != null
-                                && buttonConfig.getIcon()
-                                .startsWith(ButtonsConstants.SYSTEM_ICON_IDENTIFIER))
-                            || mColorizeMode == 0
-                            || (buttonConfig.getIcon() != null
-                                && !buttonConfig.getIcon().equals(ButtonsConstants.ICON_EMPTY)
-                                && mColorizeMode != 1)) {
-                        Bitmap colorBitmap = ((BitmapDrawable) front).getBitmap();
-                        Bitmap grayscaleBitmap = toGrayscale(colorBitmap);
 
-                        Paint pp = new Paint();
-                        PorterDuffColorFilter frontFilter =
-                            new PorterDuffColorFilter(mColor, PorterDuff.Mode.MULTIPLY);
-                        pp.setColorFilter(frontFilter);
-                        Canvas cc = new Canvas(grayscaleBitmap);
-                        cc.drawBitmap(grayscaleBitmap, 0, 0, pp);
-
-                        iv.setImageBitmap(grayscaleBitmap);
+                // colorize system and gallery icons always and app icons only if toggled
+                if (((buttonConfig.getIcon() != null
+                            && buttonConfig.getIcon()
+                            .startsWith(ButtonsConstants.SYSTEM_ICON_IDENTIFIER))
+                        || mColorizeMode == 0
+                        || (buttonConfig.getIcon() != null
+                            && !buttonConfig.getIcon().equals(ButtonsConstants.ICON_EMPTY)
+                            && mColorizeMode != 1))
+                        && mColorizeMode != 3) {
+                        iv.setImageBitmap(ColorHelper.getColoredBitmap(front, mColor));
                     } else {
                         iv.setImageDrawable(front);
                     }
-                } catch (Exception e) {
-                        // will never happen but to be sure show the icon
-                        iv.setImageDrawable(front);
-                }
 
                 mGetPxPadding = (int) convertDpToPixel(5, mContext);
                 iv.setPadding(mGetPxPadding, mGetPxPadding, mGetPxPadding, mGetPxPadding);

turtlepa/android_frameworks_base-1

core/java/com/android/internal/util/djl/ColorHelper.java
@@ -1,0 +1,60 @@
+/*
+* Copyright (C) 2013 SlimRoms Project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+package com.android.internal.util.djl;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffColorFilter;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+
+public class ColorHelper {
+
+    public static Bitmap getColoredBitmap(Drawable d, int color) {
+        Bitmap colorBitmap = ((BitmapDrawable) d).getBitmap();
+        Bitmap grayscaleBitmap = toGrayscale(colorBitmap);
+        Paint pp = new Paint();
+        PorterDuffColorFilter frontFilter =
+            new PorterDuffColorFilter(color, PorterDuff.Mode.MULTIPLY);
+        pp.setColorFilter(frontFilter);
+        Canvas cc = new Canvas(grayscaleBitmap);
+        cc.drawBitmap(grayscaleBitmap, 0, 0, pp);
+        return grayscaleBitmap;
+    }
+
+    private static Bitmap toGrayscale(Bitmap bmpOriginal) {
+        int width, height;
+        height = bmpOriginal.getHeight();
+        width = bmpOriginal.getWidth();
+
+        Bitmap bmpGrayscale = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        Canvas c = new Canvas(bmpGrayscale);
+        Paint paint = new Paint();
+        ColorMatrix cm = new ColorMatrix();
+        cm.setSaturation(0);
+
+        ColorMatrixColorFilter f = new ColorMatrixColorFilter(cm);
+        paint.setColorFilter(f);
+        c.drawBitmap(bmpOriginal, 0, 0, paint);
+        return bmpGrayscale;
+    }
+
+}

core/java/com/android/internal/util/djl/TorchConstants.java
@@ -1,0 +1,56 @@
+/*
+ * Copyright (C) 2013 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.internal.util.djl;
+
+import android.content.Intent;
+
+public class TorchConstants {
+    /**
+     * Package name of the torch app
+     */
+    public static final String APP_PACKAGE_NAME = "net.cactii.flash2";
+
+    /**
+     * Intent broadcast action for toggling the torch state
+     */
+    public static final String ACTION_TOGGLE_STATE = APP_PACKAGE_NAME + ".TOGGLE_FLASHLIGHT";
+
+    /**
+     * Extra for {@link ACTION_TOGGLE_STATE}:
+     * When toggling to on, use the bright brightness setting
+     * Type: boolean
+     */
+    public static final String EXTRA_BRIGHT_MODE = "bright";
+
+    /**
+     * Intent action for 'torch state changed' broadcast
+     */
+    public static final String ACTION_STATE_CHANGED = APP_PACKAGE_NAME + ".TORCH_STATE_CHANGED";
+
+    /**
+     * Extra for {@link ACTION_STATE_CHANGED}:
+     * Current torch state
+     * Type: integer (0/1)
+     */
+    public static final String EXTRA_CURRENT_STATE = "state";
+
+    /**
+     * Intent for launching the torch application
+     */
+    public static Intent INTENT_LAUNCH_APP = new Intent(Intent.ACTION_MAIN)
+            .setClassName(APP_PACKAGE_NAME, APP_PACKAGE_NAME + ".MainActivity");
+}

packages/SystemUI/src/com/android/systemui/statusbar/phone/ShortcutsWidget.java
@@ -20,23 +20,9 @@ import android.app.ActivityManagerNative;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.ActivityInfo;
-import android.content.pm.PackageManager;
-import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.res.Configuration;
-import android.content.res.Resources;
-import android.content.res.Resources.NotFoundException;
 import android.database.ContentObserver;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
-import android.graphics.ColorMatrix;
-import android.graphics.ColorMatrixColorFilter;
-import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
-import android.graphics.Paint;
-import android.graphics.PorterDuff;
-import android.graphics.PorterDuffColorFilter;
 import android.provider.Settings;
 import android.net.Uri;
 import android.os.Bundle;
@@ -60,6 +46,8 @@ import android.widget.LinearLayout;
 import com.android.internal.util.djl.ButtonsHelper;
 import com.android.internal.util.djl.ButtonConfig;
 import com.android.internal.util.djl.ButtonsConstants;
+import com.android.internal.util.djl.ColorHelper;
+
 import com.android.systemui.R;
 
 import java.io.File;
@@ -275,23 +263,6 @@ public class ShortcutsWidget extends LinearLayout {
         requestLayout();
     }
 
-    private Bitmap toGrayscale(Bitmap bmpOriginal) {
-        int width, height;
-        height = bmpOriginal.getHeight();
-        width = bmpOriginal.getWidth();
-
-        Bitmap bmpGrayscale = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
-        Canvas c = new Canvas(bmpGrayscale);
-        Paint paint = new Paint();
-        ColorMatrix cm = new ColorMatrix();
-        cm.setSaturation(0);
-
-        ColorMatrixColorFilter f = new ColorMatrixColorFilter(cm);
-        paint.setColorFilter(f);
-        c.drawBitmap(bmpOriginal, 0, 0, paint);
-        return bmpGrayscale;
-    }
-
     public void recreateShortcutLayout() {
         removeAllViews();
         determineMargins();
@@ -310,33 +281,20 @@ public class ShortcutsWidget extends LinearLayout {
 
                 // Draw ImageView
                 ImageView iv = new ImageView(mContext);
-                try {
-                    // colorize system and gallery icons always and app icons only if toggled
-                    if ((buttonConfig.getIcon() != null
-                                && buttonConfig.getIcon()
-                                .startsWith(ButtonsConstants.SYSTEM_ICON_IDENTIFIER))
-                            || mColorizeMode == 0
-                            || (buttonConfig.getIcon() != null
-                                && !buttonConfig.getIcon().equals(ButtonsConstants.ICON_EMPTY)
-                                && mColorizeMode != 1)) {
-                        Bitmap colorBitmap = ((BitmapDrawable) front).getBitmap();
-                        Bitmap grayscaleBitmap = toGrayscale(colorBitmap);
 
-                        Paint pp = new Paint();
-                        PorterDuffColorFilter frontFilter =
-                            new PorterDuffColorFilter(mColor, PorterDuff.Mode.MULTIPLY);
-                        pp.setColorFilter(frontFilter);
-                        Canvas cc = new Canvas(grayscaleBitmap);
-                        cc.drawBitmap(grayscaleBitmap, 0, 0, pp);
-
-                        iv.setImageBitmap(grayscaleBitmap);
+                // colorize system and gallery icons always and app icons only if toggled
+                if (((buttonConfig.getIcon() != null
+                            && buttonConfig.getIcon()
+                            .startsWith(ButtonsConstants.SYSTEM_ICON_IDENTIFIER))
+                        || mColorizeMode == 0
+                        || (buttonConfig.getIcon() != null
+                            && !buttonConfig.getIcon().equals(ButtonsConstants.ICON_EMPTY)
+                            && mColorizeMode != 1))
+                        && mColorizeMode != 3) {
+                        iv.setImageBitmap(ColorHelper.getColoredBitmap(front, mColor));
                     } else {
                         iv.setImageDrawable(front);
                     }
-                } catch (Exception e) {
-                        // will never happen but to be sure show the icon
-                        iv.setImageDrawable(front);
-                }
 
                 mGetPxPadding = (int) convertDpToPixel(5, mContext);
                 iv.setPadding(mGetPxPadding, mGetPxPadding, mGetPxPadding, mGetPxPadding);