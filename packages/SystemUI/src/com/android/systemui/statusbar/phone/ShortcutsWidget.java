e918f6af849b9272e1ab81c1b0dbb4d7c86f0896
David LaMontagne <DJLaMontagneIII@gmail.com>
2013-11-16 20:51:32 -0500

    Use DJL Imports

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/ShortcutsWidget.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/ShortcutsWidget.java
@@ -57,9 +57,9 @@ import android.widget.HorizontalScrollView;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
-import com.android.internal.util.slim.ButtonsHelper;
-import com.android.internal.util.slim.ButtonConfig;
-import com.android.internal.util.slim.ButtonsConstants;
+import com.android.internal.util.djl.ButtonsHelper;
+import com.android.internal.util.djl.ButtonConfig;
+import com.android.internal.util.djl.ButtonsConstants;
 import com.android.systemui.R;
 
 import java.io.File;
