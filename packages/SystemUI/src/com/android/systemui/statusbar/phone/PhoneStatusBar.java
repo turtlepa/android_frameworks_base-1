1a2376a6dcc83a62234146a0d3dcb2f1fee6729f
Lars Greiss <kufikugel@googlemail.com>
2013-12-16 14:10:37 +0100

    systemUI: split the broadcast receiver in phonestatusbar
    The current filters we need do not play nice together and
    cause excesive delays on receiving the screen on and off
    broadcast. Split them to have both proper working. This solves
    as well reported issue that notification pannel stays open
    
    Change-Id: I1075d3c6a7e8c59ea158dee8f8b622e78b651622

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -1071,13 +1071,17 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode {
         filter.addAction(Intent.ACTION_SCREEN_OFF);
         filter.addAction(Intent.ACTION_SCREEN_ON);
         filter.addAction(ACTION_DEMO);
+        context.registerReceiver(mBroadcastReceiver, filter);
+
+        // receive broadcasts for app actions
+        filter = new IntentFilter();
         filter.addAction(Intent.ACTION_EXTERNAL_APPLICATIONS_AVAILABLE);
         filter.addAction(Intent.ACTION_EXTERNAL_APPLICATIONS_UNAVAILABLE);
         filter.addAction(Intent.ACTION_PACKAGE_REMOVED);
         filter.addAction(Intent.ACTION_PACKAGE_CHANGED);
         filter.addAction(Intent.ACTION_PACKAGE_ADDED);
         filter.addDataScheme("package");
-        context.registerReceiver(mBroadcastReceiver, filter);
+        context.registerReceiver(mAppBroadcastReceiver, filter);
 
         // listen for USER_SETUP_COMPLETE setting (per-user)
         resetUserSetupObserver();
@@ -3261,7 +3265,14 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode {
                         }
                     }
                 }
-            } else if (Intent.ACTION_EXTERNAL_APPLICATIONS_UNAVAILABLE.equals(action)
+            }
+        }
+    };
+
+    private BroadcastReceiver mAppBroadcastReceiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (Intent.ACTION_EXTERNAL_APPLICATIONS_UNAVAILABLE.equals(action)
                 || Intent.ACTION_EXTERNAL_APPLICATIONS_AVAILABLE.equals(action)
                 || Intent.ACTION_PACKAGE_REMOVED.equals(action)
                 || Intent.ACTION_PACKAGE_CHANGED.equals(action)
@@ -3620,6 +3631,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode {
             mWindowManager.removeViewImmediate(mNavigationBarView);
         }
         mContext.unregisterReceiver(mBroadcastReceiver);
+        mContext.unregisterReceiver(mAppBroadcastReceiver);
     }
 
     private boolean mDemoModeAllowed;
