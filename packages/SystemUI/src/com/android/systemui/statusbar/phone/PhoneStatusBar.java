f995025ea1e4a814d6f25e1442413c1f1ff53af3
Lars Greiss <kufikugel@googlemail.com>
2013-12-21 18:38:57 +0100

    fb: lock statusbar recreation and release after ticker is done
    In rare cases it happens that the ticker call just happened and a recreation
    was called which results in a crah due of the missing binder.
    To avoid it lock the recreation when a ticker call is in progress
    and release it after it is done and notify the recreation to go ahead
    
    Change-Id: Iedbf858bdb2ac2ad97563e344a571b384e17cfc7

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -258,6 +258,10 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode {
 
     private boolean mShowCarrierInPanel = false;
 
+    private boolean mRecreating = false;
+    private boolean mTickerInProgress = false;
+    private final Object mLock = new Object();
+
     // position
     int[] mPositionTmp = new int[2];
     boolean mExpandedVisible;
@@ -314,8 +318,6 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode {
     int mLinger;
     int mInitialTouchX;
     int mInitialTouchY;
-
-    private boolean mRecreating = false;
 
     // for disabling the status bar
     int mDisabled = 0;
@@ -2832,15 +2834,21 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode {
         // not for you
         if (!notificationIsForCurrentUser(n)) return;
 
-        // Show the ticker if one is requested. Also don't do this
-        // until status bar window is attached to the window manager,
-        // because...  well, what's the point otherwise?  And trying to
-        // run a ticker without being attached will crash!
-        if (n.getNotification().tickerText != null && mStatusBarWindow.getWindowToken() != null) {
-            if (0 == (mDisabled & (StatusBarManager.DISABLE_NOTIFICATION_ICONS
-                            | StatusBarManager.DISABLE_NOTIFICATION_TICKER))) {
-                mTicker.addEntry(n);
+        mTickerInProgress = true;
+        synchronized(mLock) {
+            // Show the ticker if one is requested. Also don't do this
+            // until status bar window is attached to the window manager,
+            // because...  well, what's the point otherwise?  And trying to
+            // run a ticker without being attached will crash!
+            if (n.getNotification().tickerText != null
+                        && mStatusBarWindow.getWindowToken() != null) {
+                if (0 == (mDisabled & (StatusBarManager.DISABLE_NOTIFICATION_ICONS
+                                | StatusBarManager.DISABLE_NOTIFICATION_TICKER))) {
+                    mTicker.addEntry(n);
+                }
             }
+            mTickerInProgress = false;
+            mLock.notifyAll();
         }
     }
 
@@ -3399,6 +3407,15 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode {
 
     private void recreateStatusBar(boolean recreateNavigationBar) {
         mRecreating = true;
+        synchronized(mLock){
+            while (mTickerInProgress){
+                try {
+                    mLock.wait();
+                } catch (InterruptedException e) {
+                    // bad bad
+                }
+            }
+        }
         mStatusBarContainer.removeAllViews();
         mStatusBarView.postInvalidate();
         updateDisplaySize();
