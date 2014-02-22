SlimRoms/frameworks_base

Frameworks: TRDS fix possible UI freeze on auto mode change
- only call on theme mode change when auto mode changes. Under special conditions
  it can happen that it is called twice
- ensure that whenever a mode change is really done to close the notification shade
- minor cleanup

Change-Id: If1ffd4c1d2c1d8b17b6e7867a42bad1776e82c0c

core/java/com/android/internal/util/slim/SlimActions.java
@@ -83,14 +83,15 @@ public class SlimActions {
             }
 
             if (collapseShade) {
-                    if (!action.equals(ButtonsConstants.ACTION_QS)
-                            && !action.equals(ButtonsConstants.ACTION_NOTIFICATIONS)
-                            && !action.equals(ButtonsConstants.ACTION_TORCH)) {
-                        try {
-                            barService.collapsePanels();
-                        } catch (RemoteException ex) {
-                        }
+                if (!action.equals(ButtonsConstants.ACTION_QS)
+                        && !action.equals(ButtonsConstants.ACTION_NOTIFICATIONS)
+                        && !action.equals(ButtonsConstants.ACTION_THEME_SWITCH)
+                        && !action.equals(ButtonsConstants.ACTION_TORCH)) {
+                    try {
+                        barService.collapsePanels();
+                    } catch (RemoteException ex) {
                     }
+                }
             }
 
             // process the actions
@@ -166,13 +167,17 @@ public class SlimActions {
                         expandDesktopModeOn ? 0 : 1, UserHandle.USER_CURRENT);
                 return;
             } else if (action.equals(ButtonsConstants.ACTION_THEME_SWITCH)) {
-                boolean enabled = Settings.Secure.getIntForUser(
+                boolean autoLightMode = Settings.Secure.getIntForUser(
                         context.getContentResolver(),
                         Settings.Secure.UI_THEME_AUTO_MODE, 0,
-                        UserHandle.USER_CURRENT) != 1;
+                        UserHandle.USER_CURRENT) == 1;
                 boolean state = context.getResources().getConfiguration().uiThemeMode
                         == Configuration.UI_THEME_MODE_HOLO_DARK;
-                if (!enabled) {
+                if (autoLightMode) {
+                    try {
+                        barService.collapsePanels();
+                    } catch (RemoteException ex) {
+                    }
                     Toast.makeText(context,
                             com.android.internal.R.string.theme_auto_switch_mode_error,
                             Toast.LENGTH_LONG).show();

turtlepa/android_frameworks_base-1

core/java/com/android/internal/util/djl/SlimActions.java
@@ -77,11 +77,14 @@ public class SlimActions {
             } catch (RemoteException e) {
             }
 
-            if (!action.equals(ButtonsConstants.ACTION_QS)
-                    && !action.equals(ButtonsConstants.ACTION_NOTIFICATIONS)) {
-                try {
-                    barService.collapsePanels();
-                } catch (RemoteException ex) {
+            if (collapseShade) {
+                if (!action.equals(ButtonsConstants.ACTION_QS)
+                        && !action.equals(ButtonsConstants.ACTION_NOTIFICATIONS)
+                        && !action.equals(ButtonsConstants.ACTION_THEME_SWITCH)) {
+                    try {
+                        barService.collapsePanels();
+                    } catch (RemoteException ex) {
+                    }
                 }
             }
 
@@ -126,13 +129,17 @@ public class SlimActions {
                         expandDesktopModeOn ? 0 : 1, UserHandle.USER_CURRENT);
                 return;
             } else if (action.equals(ButtonsConstants.ACTION_THEME_SWITCH)) {
-                boolean enabled = Settings.Secure.getIntForUser(
+                boolean autoLightMode = Settings.Secure.getIntForUser(
                         context.getContentResolver(),
                         Settings.Secure.UI_THEME_AUTO_MODE, 0,
-                        UserHandle.USER_CURRENT) != 1;
+                        UserHandle.USER_CURRENT) == 1;
                 boolean state = context.getResources().getConfiguration().uiThemeMode
                         == Configuration.UI_THEME_MODE_HOLO_DARK;
-                if (!enabled) {
+                if (autoLightMode) {
+                    try {
+                        barService.collapsePanels();
+                    } catch (RemoteException ex) {
+                    }
                     Toast.makeText(context,
                             com.android.internal.R.string.theme_auto_switch_mode_error,
                             Toast.LENGTH_SHORT).show();