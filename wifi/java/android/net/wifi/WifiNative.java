41d52ed8e4fd8410d17d42401da2d1ae32fe394e
David LaMontagne <DJLaMontagneIII@gmail.com>
2014-06-02 21:41:18 -0400

    Revert "Wi-Fi: Ping the driver for wifi-only devices"
    This reverts commit 174f87d66144002e5b74ab88251363ca95d146d1.

--- wifi/java/android/net/wifi/WifiNative.java
+++ wifi/java/android/net/wifi/WifiNative.java
@@ -463,10 +463,6 @@ public class WifiNative {
     }
 
     public boolean setCountryCode(String countryCode) {
-        if (countryCode == null) {
-            // Ping the driver
-            return doBooleanCommand("DRIVER COUNTRY");
-        }
         return doBooleanCommand("DRIVER COUNTRY " + countryCode.toUpperCase(Locale.ROOT));
     }
 
